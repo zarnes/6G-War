ef35ec4a7cecf280e74cb9a3551e97bcce5b0ad7
Aluok <humbert.timede@gmail.com>
2018-01-28 15:03:06 +0100

    iMenus

--- Assets/Scripts/GameMenuManager.cs
+++ Assets/Scripts/GameMenuManager.cs
@@ -1,17 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameMenuManager : MonoBehaviour {
 
     private int step = 0;
     public Canvas MainMenu;
     public Canvas StartMenu;
+    public Canvas CreditMenu;
     public PlayerController playerController;
 
+    public List<Sprite> sprites;
+    public List<Sprite> bodySprites;
+    public List<Sprite> wingsSprites;
+
     // Use this for initialization
-    void Start () {
+    void Start ()
+    {
+        CreditMenu.enabled = false;
         StartMenu.enabled = false;
+        MainMenu.enabled = true;
         this.playerController.Init();
 
     }
@@ -22,21 +32,43 @@ public class GameMenuManager : MonoBehaviour {
         List<Player> players = this.playerController.players;
         if (StartMenu.enabled == true)
         {
-            /*
-            float a = Input.GetAxis("A" + i);
+            for (int i = 1; i <= players.Count; i++)
+            {
+                /**
+                float a = Input.GetAxis("A" + i);
 
-            if (a > 0.1f && !player.buyFlag) ;
-            //*/
-        } else
+                if (a > 0.1f)
+                {
+                    players[i - 1].bodySprite = this.bodySprites[];
+                }
+                this.transform.Find("Body" + i).GetComponent<Image>().sprite = players[i - 1].bodySprite;
+                this.transform.Find("Wings" + i).GetComponent<Image>().sprite = players[i - 1].wingsSprite;
+    //*/
+            }
+
+            if (Input.GetAxis("Start") > 0.1f)
+            {
+                SceneManager.LoadScene("main");
+            }
+        }
+        else
         {
             for (int i = 1; i <= players.Count; i++)
             {
-                float a = Input.GetAxis("Start" + i);
+                float a = Input.GetAxis("A" + i);
                 if (a > 0.1f)
                 {
                     StartMenu.enabled = true;
                     MainMenu.enabled = false;
+                    CreditMenu.enabled = false;
                 }
+                float b = Input.GetAxis("B" + i);
+                if (b > 0.2f )
+                {
+                    CreditMenu.enabled = !CreditMenu.enabled;
+                    StartMenu.enabled = false;
+                    MainMenu.enabled = !MainMenu.enabled;
+                } 
             }
         }
 
